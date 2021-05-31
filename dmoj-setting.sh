8a27c13f7ce24bfff906ed47bfc4138aae09d2ee
CH0213 <cndghks0213@gmail.com>
2021-05-31 02:03:12 +0000

    사용법 작성

---
+++ dmoj-setting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+# Please run it before you run the script.
+# 1. sudo passwd
+# 2. su
+# 3. echo 'kch ALL=NOPASSWD: ALL' >> /etc/sudoers
+#            ┗ 자신의 계정명
+# 4. exit
+
+# Please run it after you run the script.
+# 1. su
+# 2. sed '$ d' /etc/sudoers -i
+# 3. exit
+
