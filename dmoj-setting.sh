59a55f75929d20448e17500973b0d04a2dd44e37
CH0213 <cndghks0213@gmail.com>
2021-06-04 03:11:02 +0000

    Installing the prerequisites

--- dmoj-setting.sh
+++ dmoj-setting.sh
@@ -14,3 +14,10 @@
 # 2. sed '$ d' /etc/sudoers -i
 # 3. exit
 
+## Installing the prerequisites
+echo "======================Installing the prerequisites======================"
+sudo apt-get update
+sudo apt-get install -y git gcc g++ make python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev gettext curl redis-server
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g sass postcss-cli postcss autoprefixer
