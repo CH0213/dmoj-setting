36bf60600bace4c7ffcc1e60ae68211646b752cd
CH0213 <cndghks0213@gmail.com>
2021-06-11 05:01:01 +0000

    Installing prerequisites

--- dmoj-setting.sh
+++ dmoj-setting.sh
@@ -29,3 +29,14 @@ sudo apt-get install -y mariadb-server libmysqlclient-dev
 sudo mysql -e "CREATE DATABASE dmoj DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci"
 sudo mysql -e "GRANT ALL PRIVILEGES ON dmoj.* to 'dmoj'@'localhost' IDENTIFIED BY '1234'" # 비밀번호를 설정하세요. ex) 1234
 
+## Installing prerequisites
+echo "======================Installing prerequisites========================="
+sudo apt-get install -y python3-venv
+python3 -m venv dmojsite
+. dmojsite/bin/activate
+
+git clone https://github.com/DMOJ/site.git
+cd site
+git checkout v2.1.0  # only if planning to install a judge from PyPI, otherwise skip this step
+git submodule init
+git submodule update
